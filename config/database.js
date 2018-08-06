b06e51ae84817acb9f5cd4f5c9aa010473ee4cab
Richard Perrett <richard@richardperrett.com>
2018-08-06 21:31:29 +0100

    hooked up to mLabs db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    database: 'mongodb://localhost:27017/repairbase',
+    database: 'mongodb://pecker:pecker64@ds113692.mlab.com:13692/db_repairbase',
     secret: 'OurSecret'
 
 }
